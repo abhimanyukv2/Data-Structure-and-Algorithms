789eddd6a02e79a89c1c2660f3a3d039beaa9b6f
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-10-06 19:56:48 +0530

    Binary search

---
+++ 08_BinarySearch/070_BinarySearch.py
@@ -0,0 +1,25 @@
+def binarySearch(arr,size,key):
+    start = 0
+    end = size - 1
+    mid = (start+end)//2
+
+    while start<=end:
+        if arr[mid] == key:
+            return mid
+        if key > arr[mid]:
+            start = mid + 1
+            mid = (start + end)//2
+        else:
+            end = mid - 1
+            mid = (start + end)//2
+    return -1
+
+if __name__ == "__main__":
+    even = [2,4,6,8,12,18]
+    odd = [3,8,11,14,16]
+
+    evenIndex = binarySearch(even,len(even),20)
+    print('index of 20 is',evenIndex)
+
+    oddIndex = binarySearch(odd,len(odd),8)
+    print('index of 8 is',oddIndex)
