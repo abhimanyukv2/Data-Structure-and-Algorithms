03ce36e23f5ca06e6ee9680db53a3a2643de648f
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-11-02 13:56:12 +0530

    modified

--- 08_BinarySearch/077_SquareRootUsingBinarySearch.py
+++ 08_BinarySearch/077_SquareRootUsingBinarySearch.py
@@ -1,6 +1,12 @@
-'''Yiu are given a positive integer N.
+'''You are given a positive integer N.
 Your task is to find and return its square root.
 Is N is not a perfect square, then return floor value of sqrt(N).'''
+
+'''Leetcode - 69. Sqrt(x)
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+The returned integer should be non-negative as well.
+You must not use any built-in exponent function or operator.
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.'''
 
 def sqrtN(n):
     start = 0
